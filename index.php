c44a945844a4a6c457ccac49138380d2dd486736
Michael Gwinn <gwinnmi@oregonstate.edu>
2025-06-09 15:56:28 -0700

    some tests files added

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>My Database App</title>
+</head>
+<body>
+    <h1>Welcome to My App</h1>
+    <?php
+    // Your PHP code here
+    echo "Today is " . date("Y-m-d");
+    ?>
+</body>
+</html>
